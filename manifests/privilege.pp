f827acc103afea9f8127d050cf0dcc50632b2c1d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-12-21 19:43:15 +0100

    Refactor influxdb::privilege
    This change factors out the auth to reduce duplication and tightens the
    data types.

--- manifests/privilege.pp
+++ manifests/privilege.pp
@@ -1,58 +1,41 @@
 # == Class: influxdb::privilege
 #
 define influxdb::privilege (
+  String[1] $db_user,
+  String[1] $db_name,
   Enum['absent', 'present'] $ensure       = present,
-  $db_user                                = undef,
-  $db_name                                = undef,
   Enum['ALL', 'READ', 'WRITE'] $privilege = 'ALL',
-  $https_enable                           = $influxdb::https_enable,
-  $http_auth_enabled                      = $influxdb::http_auth_enabled,
-  $admin_username                         = $influxdb::admin_username,
-  $admin_password                         = $influxdb::admin_password
+  Boolean $https_enable                   = $influxdb::https_enable,
+  Boolean $http_auth_enabled              = $influxdb::http_auth_enabled,
+  Optional[String[1]] $admin_username     = $influxdb::admin_username,
+  Optional[String[1]] $admin_password     = $influxdb::admin_password
 ) {
   if $https_enable {
-    $cmd = 'influx -ssl -unsafeSsl'
+    $args_https = ' -ssl -unsafeSsl'
   } else {
-    $cmd = 'influx'
+    $args_https = ''
   }
+
+  if $http_auth_enabled {
+    $args_auth = " -username ${admin_username} -password '${admin_password}'"
+  } else {
+    $args_auth = ''
+  }
+
+  $cmd = "influx${args_https}${args_auth}"
+
   $matches = "grep ${db_name} | grep ${privilege}"
-  if ($ensure == 'absent') and ($http_auth_enabled == true) {
+  if $ensure == 'absent' {
     exec { "revoke_${privilege}_on_${db_name}_to_${db_user}":
       path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
-      command =>
-        "${cmd} -username ${admin_username} -password '${admin_password}' \
-         -execute 'REVOKE ${privilege} ON \"${db_name}\" TO \"${db_user}\"'",
-      onlyif  =>
-        "${cmd} -username ${admin_username} -password '${admin_password}'\
-        -execute  'SHOW GRANTS FOR \"${db_user}\"' | ${matches}"
+      command => "${cmd} -execute 'REVOKE ${privilege} ON \"${db_name}\" TO \"${db_user}\"'",
+      onlyif  => "${cmd} -execute 'SHOW GRANTS FOR \"${db_user}\"' | ${matches}",
     }
-  } elsif ($ensure == 'present') and ($http_auth_enabled == true) {
+  } elsif $ensure == 'present' {
     exec { "grant_${privilege}_on_${db_name}_to_${db_user}":
       path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
-      command =>
-        "${cmd} -username ${admin_username} -password '${admin_password}' \
-        -execute 'GRANT ${privilege} ON \"${db_name}\" TO \"${db_user}\"'",
-      unless  =>
-        "${cmd} -username ${admin_username} -password '${admin_password}' \
-        -execute 'SHOW GRANTS FOR \"${db_user}\"' | ${matches}"
-    }
-  } elsif ($ensure == 'absent') and ($http_auth_enabled == false) {
-    exec { "revoke_${privilege}_on_${db_name}_to_${db_user}":
-      path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
-      command =>
-        "${cmd} -execute 'REVOKE ${privilege} ON \"${db_name}\" \
-        TO \"${db_user}\"'",
-      onlyif  =>
-        "${cmd} -execute  'SHOW GRANTS FOR \"${db_user}\"' | ${matches}"
-    }
-  } elsif ($ensure == 'present') and ($http_auth_enabled == false) {
-    exec { "grant_${privilege}_on_${db_name}_to_${db_user}":
-      path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
-      command =>
-        "${cmd} -execute 'GRANT ${privilege} ON \"${db_name}\" \
-        TO \"${db_user}\"'",
-      unless  =>
-        "${cmd} -execute 'SHOW GRANTS FOR \"${db_user}\"' | ${matches}"
+      command => "${cmd} -execute 'GRANT ${privilege} ON \"${db_name}\" TO \"${db_user}\"'",
+      unless  => "${cmd} -execute 'SHOW GRANTS FOR \"${db_user}\"' | ${matches}",
     }
   }
 }
